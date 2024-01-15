be9460b4c5417811b1a14db246360568ebd121e6
Silas  Rangel <86634935+silasrangel@users.noreply.github.com>
2024-01-15 11:34:50 -0300

    Add files via upload

---
+++ Calculadora de 4 operações/index.js
@@ -0,0 +1,20 @@
+const entrada1 = prompt("Informe oprimeiro número:")
+const entrada2 = prompt("Informe o segundo número:")
+
+const x = parseFloat(entrada1)
+const y = parseFloat(entrada2)
+
+const soma = x + y
+const subtração = x - y
+const multiplicação = x * y
+const divisão = x / y
+
+alert( 
+    "Resultados:\n" +
+    "\nSoma: " + soma +
+    "\nSubtração: " + subtração +
+    "\nMultiplicação: " + multiplicação +
+    "\nDivisão: " + divisão
+)
+
+
